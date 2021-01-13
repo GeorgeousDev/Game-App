afc0f366d645ea981ceb250ae074c2094862c97c
Jerzy Pędlowski <jerzy.pedlowski@gmail.com>
2021-01-13 13:56:01 +0100

    Add api.js Created url for popular games,

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-function App() {
-  return (
-    <div>
-      <h1>
-        Hello Ignite!
-     </h1>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>Hello Ignite!</h1>
+		</div>
+	);
+};
 
 export default App;
