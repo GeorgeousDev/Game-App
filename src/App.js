9caaf2501227fe6d60c330afc073c98ebe75c62d
Jerzy Pędlowski <jerzy.pedlowski@gmail.com>
2021-01-16 12:21:04 +0100

    Fetching all datas from Api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gameActions";
+
 const App = () => {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(loadGames());
+	});
+
 	return (
 		<div>
 			<h1>Hello Ignite!</h1>
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import gamesReducer from './gameReducer';
+
+const rootReducer = combineReducers({
+    games: gamesReducer,
+})
+export default rootReducer;
