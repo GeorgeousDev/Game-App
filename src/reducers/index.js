fee7a47be2f665729dc701b69700a5a0ca31bebd
Jerzy Pędlowski <jerzy.pedlowski@gmail.com>
2021-01-27 12:59:46 +0100

    Updates

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import gamesReducer from './gameReducer';
-import detailReducer from './DetailReducer';
-
-const rootReducer = combineReducers({
-    games: gamesReducer,
-    detail: detailReducer,
-})
-export default rootReducer;
